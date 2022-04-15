905131cdc61c4100c12622350b0698923934c821
yskuridov <yskuridov@gmail.com>
2022-04-14 23:35:27 -0400

    Project initialized

---
+++ tp3spring/src/test/java/com/skuridov/tp3/tp3spring/Tp3springApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skuridov.tp3.tp3spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp3springApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

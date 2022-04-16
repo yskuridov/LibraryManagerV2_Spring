f2b8cd9d85de419b69d6ef2334ebae565d1eebc4
yskuridov <yskuridov@gmail.com>
2022-04-16 02:04:12 -0400

    ReturnDocument implemented + functions required for cleaner code

--- tp3spring/src/main/java/com/skuridov/tp3/tp3spring/service/MemberService.java
+++ tp3spring/src/main/java/com/skuridov/tp3/tp3spring/service/MemberService.java
@@ -3,12 +3,14 @@ package com.skuridov.tp3.tp3spring.service;
 import com.skuridov.tp3.tp3spring.dto.Document.BookForm;
 import com.skuridov.tp3.tp3spring.model.Document.Book;
 import com.skuridov.tp3.tp3spring.model.Document.Document;
+import com.skuridov.tp3.tp3spring.model.Fine.Fine;
 import com.skuridov.tp3.tp3spring.model.Loan.Loan;
 import com.skuridov.tp3.tp3spring.model.User.Member;
 import com.skuridov.tp3.tp3spring.repository.*;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -54,23 +56,63 @@ public class MemberService {
     }
 
     public void loanDocument(long memberId, long documentId) throws Exception {
-        Optional<Member> memberOpt = memberRepository.findMemberByIdWithFineList(memberId);
-        Optional<Document> documentOpt = documentRepository.findById(documentId);
-        if(!memberOpt.isEmpty() && !documentOpt.isEmpty()) {
-            Member member = memberOpt.get();
-            member.setLoanList(memberRepository.findMemberByIdWithLoanList(memberId).get().getLoanList());
-            Document document = documentOpt.get();
-            Loan loan = new Loan(LocalDate.now(), LocalDate.now().plusDays(document.getLoanLength()), member, document);
-            document.setNbCopies(document.getNbCopies() - 1);
-            member.getLoanList().add(loan);
-            memberRepository.save(member);
-            documentRepository.save(document);
-            loanRepository.save(loan);
-        }
-        else{
-            throw new Exception("Entities not found");
-        }
+        Member member = getMemberFromOptional(memberId);
+        Document document = getDocumentFromOptional(documentId);
+        Loan loan = new Loan(LocalDate.now(), LocalDate.now().plusDays(document.getLoanLength()), member, document);
+        document.setNbCopies(document.getNbCopies() - 1);
+        member.getLoanList().add(loan);
+        memberRepository.save(member);
+        documentRepository.save(document);
+        loanRepository.save(loan);
     }
 
+    public void returnDocument(long memberId, long documentId) throws Exception{
+        Member member = getMemberFromOptional(memberId);
+        Document document = getDocumentFromOptional(documentId);
+        Loan loan = findLoan(member, document);
+        member.setDebt(member.getDebt() + calculateFee(loan));
+        member.getLoanList().remove(loan);
+        document.setNbCopies(document.getNbCopies() + 1);
+        memberRepository.save(member);
+        documentRepository.save(document);
+        loanRepository.delete(loan);
+    }
 
+    private Loan findLoan(Member member, Document document) throws Exception {
+        for(Loan l : member.getLoanList()){
+            if(l.getDocument().equals(document)){
+                return l;
+            }
+        }
+        throw new Exception("The document wasn't loaned by that member");
+    }
+
+    private double calculateFee(Loan loan){
+        int differenceInDays;
+        differenceInDays = (int) (ChronoUnit.DAYS.between(loan.getDateDue(), LocalDate.now()));
+        double fee = 0.00;
+        if(differenceInDays > loan.getDocument().getLoanLength()){
+            fee = differenceInDays * Fine.FINE_AMOUNT;
+        }
+        return fee;
+    }
+
+    private Member getMemberFromOptional(long id) throws Exception{
+        Optional<Member> memberOpt = memberRepository.findMemberByIdWithFineList(id);
+        if(memberOpt.isEmpty()){
+            throw new Exception("Member entity not found");
+        }
+        Member member = memberOpt.get();
+        member.setLoanList(memberRepository.findMemberByIdWithLoanList(id).get().getLoanList());
+        return member;
+    }
+
+    private Document getDocumentFromOptional(long id) throws Exception{
+        Optional<Document> documentOpt = documentRepository.findById(id);
+        if(documentOpt.isEmpty()){
+            throw new Exception("Document entity not found");
+        }
+        Document document = documentOpt.get();
+        return document;
+    }
 }

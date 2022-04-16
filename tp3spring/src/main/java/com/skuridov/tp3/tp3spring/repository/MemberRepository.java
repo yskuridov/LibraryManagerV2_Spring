605a87dce51c1a1bcb07b9a6ce4bda0405ef4744
yskuridov <yskuridov@gmail.com>
2022-04-16 01:19:49 -0400

    loanDocument method implemented + classes required for the function implemented

--- tp3spring/src/main/java/com/skuridov/tp3/tp3spring/repository/MemberRepository.java
+++ tp3spring/src/main/java/com/skuridov/tp3/tp3spring/repository/MemberRepository.java
@@ -2,10 +2,17 @@ package com.skuridov.tp3.tp3spring.repository;
 
 import com.skuridov.tp3.tp3spring.model.User.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findMemberById(long id);
+    @Query("SELECT m from Member m left join fetch m.fineList fl where m.id = :id")
+    Optional<Member> findMemberByIdWithFineList(long id);
+    @Query("SELECT m from Member m left join fetch m.loanList ll where m.id = :id")
+    Optional<Member> findMemberByIdWithLoanList(long id);
+
+
 }
--- tp3spring/src/main/java/com/skuridov/tp3/tp3spring/service/MemberService.java
+++ tp3spring/src/main/java/com/skuridov/tp3/tp3spring/service/MemberService.java
@@ -2,19 +2,24 @@ package com.skuridov.tp3.tp3spring.service;
 
 import com.skuridov.tp3.tp3spring.dto.Document.BookForm;
 import com.skuridov.tp3.tp3spring.model.Document.Book;
-import com.skuridov.tp3.tp3spring.repository.BookRepository;
-import com.skuridov.tp3.tp3spring.repository.FineRepository;
-import com.skuridov.tp3.tp3spring.repository.LoanRepository;
+import com.skuridov.tp3.tp3spring.model.Document.Document;
+import com.skuridov.tp3.tp3spring.model.Loan.Loan;
+import com.skuridov.tp3.tp3spring.model.User.Member;
+import com.skuridov.tp3.tp3spring.repository.*;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class MemberService {
     private BookRepository bookRepository;
     private LoanRepository loanRepository;
     private FineRepository fineRepository;
+    private MemberRepository memberRepository;
+    private DocumentRepository documentRepository;
 
     public List<BookForm> getBooksByTitle(String title){
         List<BookForm> booksDTO = new ArrayList<>();
@@ -46,6 +51,25 @@ public class MemberService {
             booksDTO.add(new BookForm(b));
         }
         return booksDTO;
+    }
+
+    public void loanDocument(long memberId, long documentId) throws Exception {
+        Optional<Member> memberOpt = memberRepository.findMemberByIdWithFineList(memberId);
+        Optional<Document> documentOpt = documentRepository.findById(documentId);
+        if(!memberOpt.isEmpty() && !documentOpt.isEmpty()) {
+            Member member = memberOpt.get();
+            member.setLoanList(memberRepository.findMemberByIdWithLoanList(memberId).get().getLoanList());
+            Document document = documentOpt.get();
+            Loan loan = new Loan(LocalDate.now(), LocalDate.now().plusDays(document.getLoanLength()), member, document);
+            document.setNbCopies(document.getNbCopies() - 1);
+            member.getLoanList().add(loan);
+            memberRepository.save(member);
+            documentRepository.save(document);
+            loanRepository.save(loan);
+        }
+        else{
+            throw new Exception("Entities not found");
+        }
     }
 
 
